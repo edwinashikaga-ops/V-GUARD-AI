eb82a01a77d9737ac15ead6ed5ef904d56848d10
edwinashikaga-ops <276624742+edwinashikaga-ops@users.noreply.github.com>
2026-05-03 03:17:26 -0400

    Debug: Simplify getLoginUrl to return a static string

--- client/src/const.ts
+++ client/src/const.ts
@@ -7,11 +7,5 @@ export const getLoginUrl = () => {
   const redirectUri = `${window.location.origin}/api/oauth/callback`;
   const state = btoa(redirectUri);
 
-  const url = new URL(`${oauthPortalUrl || "https://auth.manus.im"}/app-auth`);
-  url.searchParams.set("appId", appId);
-  url.searchParams.set("redirectUri", redirectUri);
-  url.searchParams.set("state", state);
-  url.searchParams.set("type", "signIn");
-
-  return url.toString();
+  return "https://auth.manus.im/app-auth";
 };
